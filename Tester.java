0294d9999a8fcccf6a60a420c52b77f1de11e063
DTipikin <TipikinJE@gmail.com>
2022-06-20 20:10:52 +0300

    Added appendShift to db

--- Tester.java
+++ Tester.java
@@ -1,21 +1,41 @@
 import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Tester {
     public static void main(String[] args) {
-        File file = new File("./logs/log1.txt");
-        if (file.exists())
-            System.out.println("This file exists");
-
-        
+        LocalDateTime shiftStart = LocalDateTime.parse("2022-08-03T17:00:00");
+        LocalDateTime shiftEnd = LocalDateTime.parse("2022-08-03T22:00:00");
+        int deliveriesNum = 30;
+        try {
+            FileWriter log = new FileWriter("./logs/log1.txt",true);
+            log.append(shiftStart + "->" + shiftEnd + " +" + deliveriesNum + " Deliveries\n");
+            log.close();
+        }
+        catch (IOException e) {
+            System.out.println("EXECPTION DETECTED!");
+            e.printStackTrace();
+        }
+        System.out.println("Would you like another log?");
         
     }    
 }
+//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14     
+//2022-06-04T12:01:03 -> 2022-06-04T20:08:35 21     
+
+//2022-06-10T17:46:53 -> 2022-06-10T21:02:45 9        
+//2022-06-11T17:35:01 -> 2022-06-11T22:51:11 14
+
+//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
+//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
+//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
 //2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
 
 /*
+ * 
  * 
  * try {
             FileWriter log = new FileWriter("./logs/log1.txt");
@@ -26,3 +46,11 @@ public class Tester {
             e.printStackTrace();
         }
  */
+
+ /*
+  * LocalDateTime friday = LocalDateTime.parse("2022-06-03T20:00:00");
+        LocalDateTime saturday = LocalDateTime.parse("2022-06-04T20:00:00");
+        int fridaytester = friday.getDayOfWeek().getValue(); //5
+        int saturdaytester = saturday.getDayOfWeek().getValue(); //6
+        System.out.println(fridaytester +"  "+ saturdaytester);
+  */
--- WageCalc.java
+++ WageCalc.java
@@ -6,25 +6,33 @@ import java.util.Scanner;
 
 /*
  * Assumptions:
- * 1. I work only on the weekends (Delivery bonus is 4 instead of 3)
+ * 1.no overtime
+ * 2.no other bonuses
  */
 public class WageCalc {
+    final int FRIDAY = 5;
+    final int SATURDAY = 6;
+    final static double  SATURDAY_WAGE_MODIFIER = 1.5;
     public static void main(String[] args) { 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 1 to calc shift's pay");
-        System.out.println("Enter 2 to calc next paycheck");
+        System.out.println("Enter 1 to enter a shift to the database");
+        System.out.println("Enter 2 to calc a specific shift's pay");
         System.out.println("Enter 0 to exit");
         int decision = scan.nextInt();
         while (decision != 0) {
             switch (decision) {
-                //get shift's pay (and put in the DATABASE)
+                //
+                case 0:
+                    break;
                 case 1:
-                    System.out.println(registerShift());
+                    System.out.println("Appending shift!");
+                    appendShift();
                     break;
                 case 2:
-                    //calcPaycheck()  Calculate next paycheck
-                    break;    
-
+                    System.out.println(calculateShift());
+                    break;
+                default:
+                    System.out.println("Please re-enter a valid choice");
 
             }
             System.out.println("Enter your next decision");
@@ -33,11 +41,15 @@ public class WageCalc {
 
         scan.close();           
     }
+
     //result is first part the hourly total pay and second part is delivery bonus sum
-    public static double registerShift() {
+    private static double calculateShift() {
+        double minutesInShift = 0; //for overtime in the future
         double totalPay = 0;
         double regPayPerMinute = 29.12 / 60;
-        final double SATURDAY_WAGE_MODIFIER = 1.5;
+        int regDelivery = 3;
+        int deliveryInWeekend = 4;
+
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter a shift starting point in the format of '2022-06-03T17:00:00' (Y-M-D)");
         String startingTime = scan.nextLine();
@@ -50,28 +62,60 @@ public class WageCalc {
         //Sets up a 18:00 object to make calculations with
         String untilSixHelper = startingTime.substring(0, 10) + "T18:00:00";
         LocalDateTime six = LocalDateTime.parse(untilSixHelper);
-        
-        //if () A morning or evening shift fork!
 
-        Duration firstDuration = Duration.between(shiftStart,six);
-        int minutesUntilSix = (int)(firstDuration.toMinutes());
-        totalPay += minutesUntilSix * regPayPerMinute;
-        
-        //Calculates how much money is made from 6 to end of shift (1.5x modifier)
-        Duration secondDuration = Duration.between(six,shiftEnd);
-        int minutesFromSix = (int)(secondDuration.toMinutes());
-        totalPay += minutesFromSix * regPayPerMinute * SATURDAY_WAGE_MODIFIER;
-
-        //Calculates how much money is made from delivery bonuses
         System.out.println("Please enter the num of deliveries done");
         int deliveriesNum = scan.nextInt();
-        totalPay += deliveriesNum * 4;
+        
+        //friday - calculates regular pay until saturday modifier turns on 6
+        if (shiftStart.getDayOfWeek().getValue() == 5) {
+            Duration fridayFirstDuration = Duration.between(shiftStart,six);
+            double minutesUntilSix = (double)(fridayFirstDuration.toMinutes());
+            totalPay += minutesUntilSix * regPayPerMinute;
+            Duration fridaySecondDuration = Duration.between(six,shiftEnd);
+            double minutesFromSix = (double)(fridaySecondDuration.toMinutes());
+            totalPay += minutesFromSix * regPayPerMinute * SATURDAY_WAGE_MODIFIER;
+            totalPay += deliveriesNum * deliveryInWeekend;
 
+        }
+        //saturday - hourly starts with modifier and the modifier ends on 6
+        else if (shiftStart.getDayOfWeek().getValue() == 6) {
+            Duration saturdayFirstDuration = Duration.between(shiftStart,six);
+            double minutesUntilSix = (double)(saturdayFirstDuration.toMinutes());
+            totalPay += minutesUntilSix * regPayPerMinute * SATURDAY_WAGE_MODIFIER;
+            Duration saturdaySecondDuration = Duration.between(six,shiftEnd);
+            double minutesFromSix = (double)(saturdaySecondDuration.toMinutes());
+            totalPay += minutesFromSix * regPayPerMinute;
+            totalPay += deliveriesNum * deliveryInWeekend;
+        }
+        else {
+            Duration shiftDuration = Duration.between(shiftStart,shiftEnd);
+            double shiftMinutes = (double)(shiftDuration.toMinutes()); 
+            totalPay += shiftMinutes * regPayPerMinute;
+            totalPay += deliveriesNum * regDelivery;
+        }
         scan.close();
-
-
-
         return totalPay;
     }
- 
+    // assumes the right format is put
+    private static void appendShift() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a shift starting point in the format of '2022-06-03T17:00:00' (Y-M-D)");
+        String startingTime = scan.nextLine();
+        System.out.println("Please enter a shift ending point in the format of '2022-06-03T20:00:00' (Y-M-D)");
+        String endingTime = scan.nextLine();
+        System.out.println("Please enter the num of deliveries done");
+        int deliveriesNum = scan.nextInt();
+        
+        try {
+            FileWriter log = new FileWriter("./logs/log1.txt",true);
+            log.append(startingTime + "->" + endingTime + " +" + deliveriesNum + " Deliveries\n");
+            log.close();
+        }
+        catch (IOException e) {
+            System.out.println("EXECPTION DETECTED!");
+            e.printStackTrace();
+        }
+        System.out.println("End of appending");
+        scan.close();
+    }
 }  
