dedcb3282d38a80867d54094cf694da4613c5892
DTipikin <TipikinJE@gmail.com>
2022-06-22 13:12:05 +0300

    Functional for now

--- Tester.java
+++ Tester.java
@@ -1,53 +1,24 @@
 import java.time.LocalDateTime;
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
 
 public class Tester {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int decision = 99;
-        while (decision != 0) {
-            System.out.println("Enter 1 to enter a shift to the database");
-            System.out.println("Enter 2 to calc a specific shift's pay");
-            System.out.println("Enter 3 to test");
-            System.out.println("Enter 0 to exit");
-            decision = scan.nextInt();
-            scan.nextLine();
-            switch (decision) {
-                //
-                case 0:
-                    break;
-                case 1:
-                    System.out.println("1");
-                    break;
-                case 2:
-                    System.out.println("2");
-                    break;
-                case 3:
-                    System.out.println("Case 3");
-                    break;    
-                default:
-                    System.out.println("Please re-enter a valid choice");
+        String line = "2022-06-10T17:46:53->2022-06-10T21:02:45 +9 Deliveries";
+        String subLine = line.substring(0,19);
+        String subLine2= line.substring(21, 40);
+        String delivery = line.substring(42,44);
+        System.out.println(subLine);
+        System.out.println(subLine2);
+        System.out.println(delivery);
 
-            }
-            
-        }
-           
     }
 }
-//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14     
-//2022-06-04T12:01:03 -> 2022-06-04T20:08:35 21     
-
-//2022-06-10T17:46:53 -> 2022-06-10T21:02:45 9        
-//2022-06-20T17:46:53->2022-06-20T21:02:45 +25 Deliveries
-
-//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
-//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
-//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
-//2022-06-03T17:35:01 -> 2022-06-03T22:51:11 14
 
 /*
  * 
--- WageCalc.java
+++ WageCalc.java
@@ -1,13 +1,15 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 
-/*
+/* @Very basic & personal wage calculater to get some experience before the big projects!
  * Assumptions:
  * 1.no overtime
  * 2.no other bonuses
+ * 
  */
 public class WageCalc {
     final static int FRIDAY = 5;
@@ -20,11 +22,11 @@ public class WageCalc {
         while (decision != 0) {
             System.out.println("Enter 1 to enter a shift to the database");
             System.out.println("Enter 2 to calc a specific shift's pay");
-            System.out.println("Enter 3 to test");
+            System.out.println("Enter 3 to calc a specific month's wage");
             System.out.println("Enter 0 to exit");
             decision = scan.nextInt();
+            scan.nextLine();
             switch (decision) {
-                //
                 case 0:
                     break;
                 case 1:
@@ -32,10 +34,12 @@ public class WageCalc {
                     appendShift();
                     break;
                 case 2:
-                    System.out.println(calculateShift());
+                    //System.out.println(calculateShift());
                     break;
                 case 3:
-                    System.out.println("Case 3");
+                    System.out.println("Which month would you like to calc? (Enter Y-M in the format of: 2022-06");
+                    String month = scan.nextLine();
+                    System.out.println("The total for " + month +"  is: " + calcMonthlyWage(month));
                     break;    
                 default:
                     System.out.println("Please re-enter a valid choice");
@@ -47,29 +51,25 @@ public class WageCalc {
     }
 
     //result is first part the hourly total pay and second part is delivery bonus sum
-    private static double calculateShift() {
+    private static double calculateShift(LocalDateTime[] data) {
         double minutesInShift = 0; //for overtime in the future
         double totalPay = 0;
         double regPayPerMinute = 29.12 / 60;
         int regDelivery = 3;
         int deliveryInWeekend = 4;
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter a shift starting point in the format of '2022-06-03T17:00:00' (Y-M-D)");
-        String startingTime = scan.nextLine();
-        LocalDateTime shiftStart = LocalDateTime.parse(startingTime);
-        
-        System.out.println("Please enter a shift ending point in the format of '2022-06-03T20:00:00' (Y-M-D)");
-        String endingTime = scan.nextLine();
-        LocalDateTime shiftEnd = LocalDateTime.parse(endingTime);
+        LocalDateTime shiftStart = data[0];
+
+        LocalDateTime shiftEnd = data[1];
         
         //Sets up a 18:00 object to make calculations with
-        String untilSixHelper = startingTime.substring(0, 10) + SATURDAY_TIME;
-        LocalDateTime six = LocalDateTime.parse(untilSixHelper);
+        int year = shiftStart.getYear();
+        int month = shiftStart.getMonthValue();
+        int day = shiftStart.getDayOfMonth();
+        LocalDateTime six = LocalDateTime.of(year ,month ,day ,18 ,0 ,0);
 
-        System.out.println("Please enter the num of deliveries done");
-        int deliveriesNum = scan.nextInt();
-        
+        int deliveriesNum = data[2].getMinute();
+
         //friday - calculates regular pay until saturday modifier turns on 6
         if (shiftStart.getDayOfWeek().getValue() == FRIDAY) {
             Duration fridayFirstDuration = Duration.between(shiftStart,six);
@@ -97,7 +97,7 @@ public class WageCalc {
             totalPay += shiftMinutes * regPayPerMinute;
             totalPay += deliveriesNum * regDelivery;
         }
-        scan.close();
+        System.out.println("For shift: "+shiftStart.toString()+" -> "+ shiftEnd + " +"+ data[2].getMinute() +" totalling - "+ totalPay + "NIS");
         return totalPay;
     }
     // assumes the right format is put
@@ -113,7 +113,10 @@ public class WageCalc {
         
         try {
             FileWriter log = new FileWriter("./logs/log1.txt",true);
-            log.append(startingTime + "->" + endingTime + " +" + deliveriesNum + " Deliveries\n");
+            if (deliveriesNum > 9)
+                log.append(startingTime + "->" + endingTime + " +" + deliveriesNum + " Deliveries\n");
+            else //pads the deliveriesnum 1 to 01
+                log.append(startingTime + "->" + endingTime + " +" + "0"+ deliveriesNum + " Deliveries\n");
             log.close();
         }
         catch (IOException e) {
@@ -122,5 +125,36 @@ public class WageCalc {
         }
         System.out.println("End of appending");
         scan.close();
+    }
+    private static LocalDateTime[] parseFromLine(String logLine) {
+        LocalDateTime[] result = new LocalDateTime[3];
+        LocalDateTime startingTime = LocalDateTime.parse(logLine.substring(0, 19));
+        LocalDateTime endingTime = LocalDateTime.parse(logLine.substring(21, 40));
+        LocalDateTime deliveryHelper = LocalDateTime.parse("2100-10-10T20:" + logLine.substring(42,44) + ":00"); //minutes is num of deliveries!
+        result[0] = startingTime;
+        result[1] = endingTime;
+        result[2] = deliveryHelper;
+        return result;
+    }
+    private static double calcMonthlyWage(String monthToCalc) {
+        double monthlyPay = 0;
+        try {
+            Scanner scanner = new Scanner(new File("./logs/log1.txt"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains(monthToCalc)) {
+                    LocalDateTime[] data = new LocalDateTime[2];
+                    data = parseFromLine(line);
+                    monthlyPay += calculateShift(data);
+
+                }
+                
+
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } 
+        return monthlyPay;
     }
 }  
